5e4d3c13cc906309f3056aa44ed953c49ff0632e
zjp-bit <zjpbit@gmail.com>
2020-04-22 22:51:25 +0900

    20200422 2251添加usebrass2

---
+++ 13.9/build/CMakeFiles/usebrass2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usebrass2.dir/usebrass2.cpp.o"
+  "usebrass2.pdb"
+  "usebrass2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usebrass2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
