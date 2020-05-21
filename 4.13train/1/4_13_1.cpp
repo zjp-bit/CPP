9ee76b131f6e8e3ef5cc9ee8fecf41081eaec50d
zjp <you@example.com>
2020-05-21 20:57:16 +0900

    0521 add new

---
+++ 4.13train/1/4_13_1.cpp
@@ -0,0 +1,21 @@
+/*******************************************/
+/*练习题4.13 -01
+/*
+/*
+/******************************************/
+#include <iostream>
+
+
+using namespace std;
+int main()
+{
+	const int ArSize = 20;
+	char first_name[ArSize];
+	char last_name[ArSize];
+	enum grade {A, B, C, D, F};
+	cout<<"What is your first name?";
+	cin.getline(first_name,20);
+	cout<<"What is your last name?";
+	cin.getline(last_name,20);
+
+}
