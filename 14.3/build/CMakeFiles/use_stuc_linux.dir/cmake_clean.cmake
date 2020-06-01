10b76551df6cf6a8c77b3a23b4e7ae1c9fa2c03b
zjp <you@example.com>
2020-06-01 21:48:00 +0900

    14.3 using different compiler

---
+++ 14.3/build/CMakeFiles/use_stuc_linux.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_stuc_linux.dir/use_stuc.cpp.o"
+  "use_stuc_linux.pdb"
+  "use_stuc_linux"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/use_stuc_linux.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
